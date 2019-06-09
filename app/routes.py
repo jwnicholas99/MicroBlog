3e78ea717a124d4dbcf46a14999a64c303b3a529
jwnicholas99 <jwnicholas99@gmail.com>
2019-06-08 23:14:06 -0400

    Added user authentication

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 from app import app
 from flask import render_template, flash, redirect
 from app.forms import LoginForm
+from flask import request
+from werkzeug.urls import url_parse
+from flask_login import login_required
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username':'Nicholas'}
 
@@ -24,7 +28,18 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('index'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            return redirect(url_for('index'))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
