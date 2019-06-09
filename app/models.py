907cc9c2f3dc2226b4b1b024d9e970c61ad673f3
jwnicholas99 <jwnicholas99@gmail.com>
2019-06-08 23:54:34 -0400

    Added susan as user and modified routes such that index requires login

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return checlk_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,16 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from flask import request
 from werkzeug.urls import url_parse
 from flask_login import login_required
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username':'Nicholas'}
-
     posts = [
         {
             'author': {'username':'John'},
@@ -22,10 +22,12 @@ def index():
         }
     ]
     
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
